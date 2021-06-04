afd5a4e547141c0d4395d97f25403f6743c3d60e
Hugo Takashi Vásquez Miranda <takashivm97@gmail.com>
2021-06-03 22:44:16 -0500

    Actualization v1.0

---
+++ Sistema_Solar/Assets/SCRIPT/Rotacion.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    public Transform target; 
+	public int speed; 
+	
+	void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject.transform;
+			Debug.Log ("RotateAround target not specified. Defaulting to parent GameObject");
+		}
+	}
+	void Update () {
+		transform.RotateAround(target.transform.position,target.transform.up,speed * - Time.deltaTime);
+	}
+}
---
+++ Sistema_Solar/Assets/SCRIPT/lookTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookTarget : MonoBehaviour {
+    public Transform target;
+
+    void Start () {
+		
+	}
+
+	void Update () {
+        transform.LookAt(target);
+	}
+}
